0ac50123c9e9af6d40b4ab3ab60de46d7e9ed372
Wei Ni <weini@bimicon.com>
2024-05-09 20:40:20 +1000

    feat: create combobox

---
+++ DynaDesignCommon/Controls/Selection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Controls
+{
+    public class Selection
+    {
+        private Selection() { }
+
+        public static ComboBox CreateComboBox(List<object> list, string displayProp)
+        {
+            ComboBox comboBox = new ComboBox();
+            comboBox.ItemsSource = list;
+            comboBox.DisplayMemberPath = displayProp;
+
+            return comboBox;
+        }
+    }
+}
