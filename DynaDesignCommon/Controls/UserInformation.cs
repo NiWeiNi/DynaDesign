5701408d539814ea6e3fb3063196f487ad03bd6b
Wei Ni <weini@bimicon.com>
2024-05-09 20:39:37 +1000

    feat: create label

---
+++ DynaDesignCommon/Controls/UserInformation.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Controls
+{
+    public class UserInformation
+    {
+        private UserInformation() { }
+
+        public static Label CreateLabel(string text)
+        {
+            Label label = new Label();
+            label.Content = text;
+
+            return label;
+        }
+    }
+}
