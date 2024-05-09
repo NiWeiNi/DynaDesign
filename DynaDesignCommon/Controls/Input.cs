c80830cbfef223e2a05307535c4529cbd03d0eb3
Wei Ni <weini@bimicon.com>
2024-05-09 20:40:08 +1000

    feat: create textbox

---
+++ DynaDesignCommon/Controls/Input.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Controls
+{
+    public class Input
+    {
+        private Input() { }
+
+        public static TextBox CreateTextBox(string text)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Text = text;
+
+            return textBox;
+        }
+    }
+}
